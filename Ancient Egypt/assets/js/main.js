8626b95926bbf5d01ad09a9e2eb35d68cbc35bb3
HayleyBetts <hayleybetts@yahoo.com.au>
2020-10-16 10:08:24 +1100

    NAme Chnages

--- Ancient Egypt/assets/js/main.js
+++ Ancient Egypt/assets/js/main.js
@@ -6,11 +6,13 @@ $(document).ready (function () {
   	$(".page").removeClass("hide");
 	});
 
+/*
 	$('.btn-Religion').click(function(){
 		$('.item').addClass('hide');
 		$('.item.Religion').removeClass('hide');
 	});
-
+	
+	
 	$('.btn-Symbols').click(function(){
 		$('.item').addClass('hide');
 		$('.item.Symbols').removeClass('hide');
@@ -30,15 +32,112 @@ $(document).ready (function () {
 		$('.item').addClass('hide');
 		$('.item.Egyptianlife').removeClass('hide');
 	});
+*/
 
-	$('.btn-Reset').click(function(){
+	$('.Click-reset').click(function(){
 		$('.item').removeClass('hide');
+		$('.caption').addClass('hide');
 	});
 
 	$('.btn-nav').click(function(){
 		$('.item').addClass('hide');
 		$('.item.Egyptianlife').removeClass('hide');
 	});
+
+	/*Symbols*/
+
+$(".btn-Symbols").click(function () {
+  $(".item").addClass("hide");
+  $(".caption").addClass("hide");
+
+  $(".item").removeClass("samesize");
+
+  $(".item.Symbols").removeClass("hide");
+  $(".item.Symbols .caption").removeClass("hide");
+
+  $(".item.Symbols").addClass("samesize");
+});
+
+/*Symbols*/
+
+/*Religion*/
+
+$(".btn-Religion").click(function () {
+  $(".item").addClass("hide");
+  $(".caption").addClass("hide");
+
+  $(".item").removeClass("samesize");
+
+  $(".item.Religion").removeClass("hide");
+  $(".item.Religion .caption").removeClass("hide");
+
+  $(".item.Religion").addClass("samesize");
+});
+
+
+
+/*Religion*/
+
+/*Rituals*/
+
+$(".btn-Rituals").click(function () {
+  $(".item").addClass("hide");
+  $(".caption").addClass("hide");
+
+  $(".item").removeClass("samesize");
+
+  $(".item.Rituals").removeClass("hide");
+  $(".item.Rituals .caption").removeClass("hide");
+
+  $(".item.Rituals").addClass("samesize");
+});
+
+
+/*Rituals*/
+
+/*EgyptianLife*/
+
+$(".btn-Egyptianlife").click(function () {
+  $(".item").addClass("hide");
+  $(".caption").addClass("hide");
+
+ $(".item").removeClass("samesize");
+
+  $(".item.Egyptianlife").removeClass("hide");
+  $(".item.Egyptianlife .caption").removeClass("hide");
+
+  $(".item.Egyptianlife").addClass("samesize");
+});
+
+/*EgyptianLife*/
+
+/*Pharaohs*/
+
+
+$(".btn-Pharaohs").click(function () {
+  $(".item").addClass("hide");
+  $(".caption").addClass("hide");
+
+  $(".item").removeClass("samesize");
+
+  $(".item.Pharaohs").removeClass("hide");
+  $(".item.Pharaohs .caption").removeClass("hide");
+
+  $(".item.Pharaohs").addClass("samesize");
+});
+
+/*Pharaohs*/
+
+
+$(".btn-Book").click(function () {
+  $(".caption").addClass("hide");
+  $(".item").addClass("hide");
+
+
+  $(".About.Book").removeClass("hide");
+  $(".About.Book .caption").removeClass("hide");
+
+});
 
 });
 
