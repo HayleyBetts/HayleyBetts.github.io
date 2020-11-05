fa64138407f397a3fc0bee7a077d0137a990488b
HayleyBetts <hayleybetts@yahoo.com.au>
2020-11-06 08:09:45 +1100

    chnages

---
+++ Notabook Assignment/assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready (function () {
+	console.log ('hello')
+
+
+
+$(".trans").click(function () {
+  $(".a1").addClass("hide");
+  $(".a2").addClass("hide");
+  $(".a3").addClass("hide");
